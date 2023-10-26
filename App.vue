008564d83b70ecaddd1ca4946a45562382f43c0b
Jinboii <148060789+Jinboii@users.noreply.github.com>
2023-10-26 20:56:34 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <RegistrationForm />
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import RegistrationForm from './components/RegistrationForm.vue';
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegistrationForm,
+    LoginForm
+  }
+};
+</script>
+
+<style>
+/* Add any global styles here */
+</style>
