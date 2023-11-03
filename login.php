30a5206d14b7c3a3b80e7deeeaef685ff811cfdc
Jinboii <148060789+Jinboii@users.noreply.github.com>
2023-11-03 19:32:49 +0800

    login

--- login.php
+++ login.php
@@ -1,14 +1,19 @@
 <?php
+session_start();
+
 require_once 'loginDAO.php';
 
 // Assume login.html has a form that submits data to this script via POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    
     $loginDAO = new LoginDAO();
     $isLoginSuccess = $loginDAO->checkLogin($_POST['username'], $_POST['password']);
 
     if ($isLoginSuccess) {
         // Redirect to a logged-in page
-        header("Location: dashboard.php");
+        $_SESSION["userid"] = $_POST['username'];
+        $_SESSION["usertype"] = "user";
+        header("Location: index.html");
     } else {
         // Redirect back to the login page with an error
         header("Location: login.html?error=login_failed");
--- userlog.php
+++ userlog.php
@@ -2,11 +2,9 @@
    header("Access-Control-Allow-Origin: *");
    session_start();
 
+   $userid = $_SESSION["userid"] ; 
+   $usertype = $_SESSION["usertype"];
    
-   // to draw the user id and type from session
-   $userid = "1234"; 
-   $usertype = "organization";  //change here to test for org and indiv
-
    $processed_data = [];
    array_push($processed_data,["userid"=> $userid,"usertype"=> $usertype]);
    $jsonObj = json_encode($processed_data);
