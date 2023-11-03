b3ae9ca6edbd144bdbcbc566c063a80af018366e
sakthivelg2022 <149075734+sakthivelg2022@users.noreply.github.com>
2023-11-03 12:44:27 +0800

    Add files via upload

---
+++ WAD Registration  Backend/passwordDAO.php
@@ -0,0 +1,36 @@
+<?php
+class PasswordDAO {
+    // Database credentials
+    private $db_host = 'localhost';
+    private $db_name = 'rebirth';
+    private $db_user = 'root';
+    private $db_password = 'root';
+
+    // Function to save the password to the database
+    public function savePassword($username, $email, $hashedPassword, $role) {
+        // Create a connection to the database
+        $conn = new mysqli($this->db_host, $this->db_user, $this->db_password, $this->db_name);
+        
+        // Check for a successful connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            return false;
+        }
+        
+        // Prepare the SQL statement to prevent SQL injection
+        $stmt = $conn->prepare("INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $username, $email, $hashedPassword, $role);
+
+        // Execute the statement and check for success
+        if ($stmt->execute()) {
+            $stmt->close();
+            $conn->close();
+            return true;
+        } else {
+            $stmt->close();
+            $conn->close();
+            return false;
+        }
+    }
+}
+?>
