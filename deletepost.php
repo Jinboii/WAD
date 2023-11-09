270c3ef799e2b738f30b965c3b0d2fefc58fa5d4
Shristi Pai <137492358+shristipai@users.noreply.github.com>
2023-11-09 17:16:23 +0800

    Update deletepost.php

--- deletepost.php
+++ deletepost.php
@@ -168,7 +168,6 @@
                                 </div>
                             </div>';
         } else {
-            echo "<h3>Post deletion was NOT successful!</h3>";
 
             echo ' <div class="container-fluid contact py-6 wow bounceInUp" data-wow-delay="0.1s">
             <div class="container">
